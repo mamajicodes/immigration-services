d79eba3543ca5eac3940431e845fb4d1ddc3210a
Sriram Rajagopalan <sriramr03@gmail.com>
2020-12-29 10:25:48 -0500

    initial commit

---
+++ src/main/java/com/mamajicodes/immigrationservices/ImmigrationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mamajicodes.immigrationservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmigrationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmigrationServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamajicodes/immigrationservices/ImmigrationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamajicodes.immigrationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmigrationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
